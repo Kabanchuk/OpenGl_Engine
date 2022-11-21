18b0f801efcf372533ba1a45b3b8b542996c7f82
Ex1l <csidleno2@gmail.com>
2022-11-21 17:26:04 +0300

    Added exit button(esc)

--- src/main.cpp
+++ src/main.cpp
@@ -3,33 +3,58 @@
 
 #include <iostream>
 
+int g_windowSizeX = 640;
+int g_windowSizeY = 480;
+
+void glfwWindowsSizeCallback(GLFWwindow* window, int width, int height) {
+    g_windowSizeX = width;
+    g_windowSizeY = height;
+    glViewport(0, 0, g_windowSizeX, g_windowSizeY);
+}
+
+void glfwKeyCallback(GLFWwindow* window, int key, int scancode, int action, int mode){
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+}
+
 int main(void)
 {
-    GLFWwindow* window;
-
+    setlocale(LC_ALL, "RUS");
     /* Initialize the library */
-    if (!glfwInit())
+    if (!glfwInit()) {
+        std::cout << "Ошибка инициализации glfw!";
         return -1;
+    }
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(g_windowSizeX, g_windowSizeY, "TANK-42", nullptr, nullptr);
     if (!window)
     {
+        std::cout << "Не удалось создать окно" << std::endl;
         glfwTerminate();
         return -1;
     }
+
+    glfwSetWindowSizeCallback(window, glfwWindowsSizeCallback);
+    glfwSetKeyCallback(window, glfwKeyCallback);
+
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 	
 	if (!gladLoadGL()){
-		std::cout << "Can't load GLAD!" << std::endl;
+		std::cout << "Не возможно загрузить GLAD!" << std::endl;
 		return -1;
 	}
+    std::cout << "Render: " << glGetString(GL_RENDERER) << std::endl;
+	std::cout << "Версия OpenGL: " << glGetString(GL_VERSION) << std::endl;
 	
-	std::cout << "OpenGL" << GLVersion.major << "."  << GLVersion.minor << std::endl;
-	
-	glClearColor(1, 1, 0, 1);
+	glClearColor(0, 0, 0, 1);
 	
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
